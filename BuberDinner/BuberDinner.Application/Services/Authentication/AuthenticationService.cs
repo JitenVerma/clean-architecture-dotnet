fd60a2221f572deddad453da51fb13f796e32e77
Jiten Verma <jitenverma9@gmail.com>
2024-04-26 15:34:01 +1000

    Jwt authentication enabled

--- BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,15 +1,32 @@
-﻿namespace BuberDinner.Application.Services.Authentication;
+﻿using BuberDinner.Application.Common.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
+        // Check if user already exists
+
+        // Create user (generate unique ID)
+        var userId = Guid.NewGuid();
+
+        // Create JWT token
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
         return new AuthenticationResult(
-            Guid.NewGuid(), 
+            userId, 
             firstName, 
             lastName, 
             email, 
-            "token");
+            token);
     }
 
     public AuthenticationResult Login(string email, string password)
